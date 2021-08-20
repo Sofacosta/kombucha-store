89d0ed756c48b7e0a7e0b6650fbb6431cfc69cc5
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-08-20 15:18:53 -0500

    Initialize project, add Header and StoreFront components, add bootstrap

---
+++ src/StoreFront.js
@@ -0,0 +1,20 @@
+import { react } from "babel-types";
+import React from "react";
+class StoreFront extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+    };
+  }
+  render() {
+
+  }
+
+  return (
+
+  );
+
+}
+
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./header";
+// import StoreFront from './StoreFront';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      {/* <KombuchaStore /> */}
+    </React.Fragment>
+  );
+}
+
+export default App;
